389cdfa272b018ef7c5972a2c663304da2b196cc
nalalalan <pham.alann@gmail.com>
2026-05-17 18:23:09 -0400

    Make RelayBrief buyer path trustworthy

--- site-config.js
+++ site-config.js
@@ -6,11 +6,11 @@ window.AO_CONFIG = {
   RELAY_INTENT_SUMMARY_URL: "https://alan-operator-backend-production.up.railway.app/api/relay/intent-summary",
   FIRST_MONEY_CHECKOUT_URL: "https://buy.stripe.com/bJe28t7Sa7DLgec29A2Nq08",
   FIRST_MONEY_PRICE_LABEL: "$1",
-  PACKET_CHECKOUT_URL: "https://buy.stripe.com/bJeaEZb4mf6de64dSi2Nq02",
-  PACKET_PRICE_LABEL: "$40",
+  PACKET_CHECKOUT_URL: "https://buy.stripe.com/bJe28t7Sa7DLgec29A2Nq08",
+  PACKET_PRICE_LABEL: "$1",
   PACKET_5_PACK_URL: "https://buy.stripe.com/28EaEZgoGf6d7HG9C22Nq03",
   WEEKLY_SPRINT_URL: "https://buy.stripe.com/8x200l0pIcY54vu8xY2Nq05",
   MONTHLY_AUTOPILOT_URL: "https://buy.stripe.com/aFaeVf0pI3nv6DC15w2Nq06",
-  SAMPLE_PDF_URL: "sample.pdf?v=20260517-trust",
+  SAMPLE_PDF_URL: "sample.pdf?v=20260517-trust2",
   CLIENT_GATE_PAGE: "client-intake.html"
 };
